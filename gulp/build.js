d0f7a19fd43d9b1cbd965a6e8340ca866538e608
Ivan Stepanuyk <i.stepanuyk@hedgehog.technology>
2017-09-29 11:58:43 +0300

    add default, watch, build

---
+++ gulp/build.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+
+
+gulp.task('build', function () {
+    runSequence('clean', 'sass',
+      ['minify', 'images', 'fonts',]
+    );
+  });
